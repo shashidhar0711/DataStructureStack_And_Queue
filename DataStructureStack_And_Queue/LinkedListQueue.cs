fa349953b14824666e25adce8927d99eeb5be79f
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 19:37:33 +0530

    [sasic] Add . To Creating the Queue and Adding Elements into Queue

---
+++ DataStructureStack_And_Queue/LinkedListQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureStack_And_Queue
+{
+    public class LinkedListQueue
+    {
+        /// <summary>
+        /// Initially head is null 
+        /// </summary>
+        Node head = null;
+
+        /// <summary>
+        /// Ens the queue.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        internal void EnQueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} pushed in to Queue", node.data);
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Queue is empty!");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureStack_And_Queue/Program.cs
+++ DataStructureStack_And_Queue/Program.cs
@@ -2,11 +2,24 @@
 
 namespace DataStructureStack_And_Queue
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating Reference of object Queue
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+
+            /// Adding elements into Queue
+            linkedListQueue.EnQueue(70);
+            linkedListQueue.EnQueue(50);
+            linkedListQueue.EnQueue(36);
+
+            /// Printing all the elements
+            linkedListQueue.Display();
         }
     }
 }
