2c2371c176313fbf4ec097612ae1e96882fb557a
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 17:42:25 +0530

    [sasic] Add . Creating a Stack Using Linked List and Adding Elements

---
+++ DataStructureStack_And_Queue/LinkedListStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureStack_And_Queue
+{
+    public class LinkedListStack
+    {
+        /// <summary>
+        /// Variables
+        /// </summary>
+        private Node top;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedListStack"/> class.
+        /// </summary>
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        /// <summary>
+        /// Pushes the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+
+            this.top = node;
+            Console.WriteLine("{0} pushed value to stack ", value);
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureStack_And_Queue/Program.cs
+++ DataStructureStack_And_Queue/Program.cs
@@ -2,11 +2,27 @@
 
 namespace DataStructureStack_And_Queue
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating Refernce of object class
+            LinkedListStack linkedListStack = new LinkedListStack();
+
+            /// Adding elements in to stack
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Values pushing into stack");
+
+            /// Printing all elements
+            linkedListStack.display();
         }
     }
 }
