cc477f89022ec1fe2fd61f94fab64d700270b29e
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 15:53:14 +0530

    Add project files.

---
+++ DataStructureStack_And_Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureStack_And_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
