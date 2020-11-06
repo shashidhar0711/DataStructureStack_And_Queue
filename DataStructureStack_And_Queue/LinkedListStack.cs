c3c12bb46476b1d033f2ed48c52aa72d07213d22
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 18:57:40 +0530

    [saisc] Add . Ability to Perform Peek and Pop Operation in Stack

---
+++ DataStructureStack_And_Queue/LinkedListStack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureStack_And_Queue
+{
+    public class LinkedListStack
+    {
+        /// <summary>
+        /// Variables
+        /// </summary>
+        private Node top;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedListStack"/> class.
+        /// </summary>
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        /// <summary>
+        /// Pushes the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        internal void push(int value)
+        {
+            /// Creating reference of object node
+            Node node = new Node(value);
+
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+
+            this.top = node;
+            Console.WriteLine("{0} pushed value to stack ", value);
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+
+        /// <summary>
+        /// Peeks this instance.
+        /// </summary>
+        internal void peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+
+        /// <summary>
+        /// Pops this instance.
+        /// </summary>
+        internal void pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+
+            Console.WriteLine("value poppedis {0}", this.top.data);
+            this.top = this.top.next;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is empty.
+        /// </summary>
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                peek();
+                pop();
+            }
+        }
+    }
+}
--- DataStructureStack_And_Queue/Program.cs
+++ DataStructureStack_And_Queue/Program.cs
@@ -2,11 +2,39 @@
 
 namespace DataStructureStack_And_Queue
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating Refernce of object class
+            LinkedListStack linkedListStack = new LinkedListStack();
+
+            /// Adding elements in to stack
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            /// Printing all elements
+            linkedListStack.display();
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Values pushing into stack");
+
+            /// It will return top element from stack without peek ie without deleteing
+            linkedListStack.peek();
+            /// Printing all element
+            linkedListStack.display();
+
+            /// It will return top element after deleting 
+            linkedListStack.pop();
+            linkedListStack.IsEmpty();
+
+            /// Printing all elements
+            linkedListStack.display();
+
         }
     }
 }
